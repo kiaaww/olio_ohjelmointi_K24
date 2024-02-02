8aae0bfb5537671fb25e647652729547dee4626f
kiaaww <155945180+kiaaww@users.noreply.github.com>
2024-02-02 12:49:30 +0000

    Update main.cpp

--- vk4_tehtava/main.cpp
+++ vk4_tehtava/main.cpp
@@ -8,13 +8,25 @@ using namespace std;
 
 int main()
 {
+    //tehtävän osa 1 main
+    /*Asunto tiedot;
+    tiedot.maarita(2,100);
+    tiedot.laskeKulutus(1);*/
 
+    //tehtävän osa 2 main
+    /*Katutaso tulos;
+    tulos.maaritaAsunnot();
+    Kerros tulos2;
+    tulos2.maaritaAsunnot();
+
+    int kokonaisuus = tulos.laskeKulutus(1) + tulos2.laskeKulutus(1);
+    cout<<"Kerrosten asuntojen kulutus, kun hinta on=1, on "<<kokonaisuus<<endl;*/
+
+    //tehtävän osa 3 main
     Kerrostalo tiedot;
-
 
     int kokonaisuus=tiedot.laskeKulutus(1);
     cout<<"asuntojen kulutus, kun hinta=1, on "<<kokonaisuus<<endl;
-
 
     return 0;
 }
